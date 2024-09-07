558533f6414ca393721129340fdf097f24cf5806
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-09-07 23:11:40 +0200

    feat: Add Multilingual Homepage
    - Add a homepage with project details.
    - Implement text sources in English (EN), French (FR), and Portuguese (PT-BR).
    - Enable language switching functionality on the site.
    - Implement WebConfig class to configure i18n language interceptor and persist the chosen language with cookie.

---
+++ src/main/java/com/api/dummydata/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.api.dummydata.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+}
